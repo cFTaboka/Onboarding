d3844f50aec930267448835879d1ac0108f5458d
cFTaboka <93674131+cFTaboka@users.noreply.github.com>
2021-11-03 15:07:17 -0400

    Test file

---
+++ testing.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class testing{
+public static void main (String[]args){
+
+int a =1;
+int b=3;
+int c = a+b;
+System.out.println ("This is a test case for commonFont");
+System.out.println("The value of C is:" + c);
+}
+}
